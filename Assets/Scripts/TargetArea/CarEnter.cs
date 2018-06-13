67c5d5506c76966e6bb80baf2f5b291f01e29444
unknown <Robert.Aron.tx@gmail.com>
2018-06-13 18:57:53 -0500

    target area manager working

--- Assets/Scripts/TargetArea/CarEnter.cs
+++ Assets/Scripts/TargetArea/CarEnter.cs
@@ -3,8 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CarEnter : MonoBehaviour {
+	//must have 2 entered to move on
+	int playersEntered = 0;
+	AreaManager areaManager;
+
+	private void Start() {
+		areaManager = transform.parent.gameObject.GetComponent<AreaManager>();
+	}
 
 	private void OnTriggerEnter(Collider other) {
-		Debug.Log("This is what entered" + other.tag);
+		if(other.CompareTag("Player")){
+			playersEntered++;
+		}
+		if(playersEntered==2){
+			areaManager.nextTarget();
+		}
+	}
+	private void OnTriggerExit(Collider other) {
+		if(other.CompareTag("Player")){
+			playersEntered--;
+		}
 	}
 }
